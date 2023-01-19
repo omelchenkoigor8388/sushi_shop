a71933abd13d556418b5803b610429da5f08cc40
Omelchenko Igor <106494942+omelchenkoigor8388@users.noreply.github.com>
2023-01-19 14:21:34 +0200

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,33 @@
+document.querySelector('.btn-primary').addEventListener('click', event =>{
+    const counterWrapper = document.querySelectorAll('.cart-item');
+
+    let sumPrise = document.querySelector('.total-price');
+    let sumPriseText =' Всього : ' + sumPrise.textContent;
+    
+    counterWrapper.forEach((el) => {
+        let name = el.querySelector('.cart-item__title');
+        let number = el.querySelector('.items__current');
+        let sum = el.querySelector('.price__currency');
+        
+        let text = name.textContent + ' \nкількість : ' + number.textContent +'шт.' + ' \nціні : ' + sum.textContent;
+
+        var token = '5610797462:AAG8xBcl76SAh3B2luqzlOGLfIMBFHQU_uE';
+        var chat_id = -881956514;
+        var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}`
+    
+        let api = new XMLHttpRequest();
+        api.open("GET", url, true);
+        api.send();
+
+    });
+
+    var token = '5610797462:AAG8xBcl76SAh3B2luqzlOGLfIMBFHQU_uE';
+    var chat_id = -881956514;
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${sumPriseText}`
+
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
+
+});
+
